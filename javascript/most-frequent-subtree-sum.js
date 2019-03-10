6a5e4bfb6c54f037f815e90a97c5e1aee1b4fc91
Sarith Slonh <dev.sslonh@gmail.com>
2019-03-10 14:11:38 -0500

    Add Most Frequent Subtree Sum

---
+++ javascript/most-frequent-subtree-sum.js
@@ -0,0 +1,33 @@
+/**
+  * Problem: 508. Most Frequent Subtree Sum
+  * Difficulty: Medium
+  * Link: https://leetcode.com/problems/most-frequent-subtree-sum/
+  * Runtime: 72 ms, faster than 100%
+  * Space: 38 MB, less than 100%
+*/
+var findFrequentTreeSum = function(root) {
+    if (!root) return [];
+    let map = new Map(), max = 0, sum, results = [];
+    
+    function traverse(node) {
+        if (!node) return 0;
+        sum = node.val + traverse(node.left) + traverse(node.right);
+        if (!map.has(sum)) {
+            map.set(sum, 1);
+        } else {
+            map.set(sum, map.get(sum) + 1);
+        }
+        return sum;
+    }
+    
+    traverse(root);
+    
+    map.forEach((val,key) => {
+       if (val > max) max = val; 
+    });
+    map.forEach((val,key) => {
+       if (val == max) results.push(key); 
+    });
+    
+    return results;
+};
